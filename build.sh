4b1ba73f7a5cc76925f4cc587d92fe0cb553a8e8
ggpwnkthx <ibjessup@gmail.com>
2021-03-11 13:38:53 -0500

    clean up

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ docker run -it --rm -v $(pwd):/src/app -w /src/app/src golang:alpine go build -o
 
 docker build -t $PLUGIN_NAME .
 CONTAINER_ID=$(docker create $PLUGIN_NAME true)
-mkdir rootfs
-docker export "$CONTAINER_ID" | tar -x -C rootfs
+mkdir -p plugin/rootfs
+docker export "$CONTAINER_ID" | tar -x -C plugin/rootfs
 docker rm -vf "$CONTAINER_ID"
 docker rmi $PLUGIN_NAME
-docker plugin create $PLUGIN_NAME .
+docker plugin create $PLUGIN_NAME ./plugin
